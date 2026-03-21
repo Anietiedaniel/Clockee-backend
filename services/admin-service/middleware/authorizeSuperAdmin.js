3ac2999b4755ab07e6e1dfeb8d671cbca1c3056b
Justice3984 <onyenmeliteobi@gmail.com>
2026-03-21 09:02:31 +0100

    debugging

--- services/admin-service/middleware/authorizeSuperAdmin.js
+++ services/admin-service/middleware/authorizeSuperAdmin.js
@@ -1,5 +1,5 @@
 export const authorizeSuperAdmin = (req, res, next) => {
-  if (!req.user.role || !req.user.role.includes("super_admin")) {
+  if (!Array.isArray(req.user.role) || !req.user.role.includes("super_admin")) {
     return res.status(403).json({ message: "Super admin access required!!" });
   }
   next();
