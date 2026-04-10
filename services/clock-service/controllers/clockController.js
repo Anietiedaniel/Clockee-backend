02c5f8a6a26171efcb1340ce0bf71aea662d615b
Justice3984 <onyenmeliteobi@gmail.com>
2026-04-10 09:50:16 +0100

    nmsks

--- services/clock-service/controllers/clockController.js
+++ services/clock-service/controllers/clockController.js
@@ -25,7 +25,7 @@ export const clockAttendance = async (req, res) => {
       overrideCode,
     } = req.body;
 
-    const userId = req.user. userId;
+    const userId = req.user.userId || req.user.id ;
 
     const userRoles = Array.isArray(req.user.role)
       ? req.user.role
