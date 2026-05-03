3f7984662da5ef0706a005f08b4173c5beda5c93
cieos2022 <onyenmeliteobi@ymail.com>
2026-05-04 00:17:01 +0100

    extended expires in

--- services/auth-service/controllers/authController.js
+++ services/auth-service/controllers/authController.js
@@ -39,7 +39,7 @@ function generateToken({ userId, sessionId, role, institutionId, name, email }) 
       email,
     },
     process.env.JWT_SECRET,
-    { expiresIn: "30d" }
+    { expiresIn: "30y" }
   );
 }
 
